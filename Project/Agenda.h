fe05afdddb2e5219367192f80ded0a575d945799
CesarPiedraG <90583996+CesarPiedraG@users.noreply.github.com>
2023-01-13 22:05:13 -0600

    Agenda.h

---
+++ Project/Agenda.h
@@ -0,0 +1,107 @@
+#ifndef AGENDA
+#define AGENDA
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <string.h>
+#define MAX 25
+
+//----------------------------------------------------------------------
+//  Fechas y Contactos
+//----------------------------------------------------------------------
+
+typedef struct
+{
+    int dia;
+    int mes;
+    int anio;
+} Fecha;
+
+typedef struct
+{
+    char contact_name[MAX];
+    int phone_number;
+    
+} Contacto;
+
+//NO tienen operaciones, accesaremos directamente a los campos
+
+
+//----------------------------------------------------------------------
+//  Lista Simplemente enlazada
+//----------------------------------------------------------------------
+
+typedef struct Node {
+
+      Fecha date;
+      Contacto contact;
+
+      struct Node* prev;
+      struct Node* next;
+} Node;
+
+
+typedef struct SLL
+{
+	Node* first;
+	Node* last;
+	Node* cursor;
+	size_t len;
+} SLL;
+
+SLL*   SLL_New();
+void   SLL_Delete( SLL** this );
+
+void   SLL_Push_front( SLL* this, int data );
+void   SLL_Push_back( SLL* this, int data );
+void   SLL_Insert( SLL* this, int data );
+
+void   SLL_Pop_front( SLL* this );
+
+int    SLL_Get( SLL* this );
+size_t SLL_Len( SLL* this );
+
+bool   SLL_Is_empty( SLL* this );
+void   SLL_Make_empty( SLL* this );
+
+void   SLL_Cursor_front( SLL* this );
+void   SLL_Cursor_back( SLL* this );
+void   SLL_Cursor_next( SLL* this );
+bool   SLL_Find( SLL* this, int key );
+
+//----------------------------------------------------------------------
+//  AGENDA
+//----------------------------------------------------------------------
+
+typedef struct
+{
+    SLL* list_fechas;
+    SLL* list_contactos;
+} Agenda; 
+
+
+Agenda* Agenda_new();
+
+void   Agenda_Delete( Agenda** this );
+
+void   Agenda_add_date( Agenda* this, Fecha data );
+void   Agenda_add_contact(Agenda* this, Contacto data );
+
+void   Agenda_Pop_front( SLL* this );
+
+bool   SLL_Is_empty( SLL* this );
+void   SLL_Make_empty( SLL* this );
+
+bool   SLL_Find( SLL* this, int key );
+int    Print_Agenda( SLL* this );
+
+
+
+
+#endif   /* ----- #ifndef STOCK_INC  ----- */
+
+int main(){
+    
+}
